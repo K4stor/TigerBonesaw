4a0452778b2221e42021f362003225193782c878
Carsten Przyluczky <kastor.rocks@gmail.com>
2019-07-20 00:11:13 +0200

    added basic controls

--- js/player.js
+++ js/player.js
@@ -10,6 +10,15 @@ function createPlaylist() {
   tracks.forEach(track => {
     var element = document.createElement('li');
     element.textContent = track;
+    element.onclick = function() {
+      var audio = document.getElementsByTagName("audio")[0];
+      if (audio.paused) {
+        audio.currentTime = 0;
+        audio.play();     
+      } else {
+        audio.pause();
+      }
+    }
     rootElement.appendChild(element);
   });
 }
