20efecefc6c1f05ca080281a1cff46e418816477
Carsten Przyluczky <kastor.rocks@gmail.com>
2019-07-27 22:50:20 +0200

    added plauer controls and parsing

--- js/player.js
+++ js/player.js
@@ -1,15 +1,21 @@
+var tracksRequestURL = './json/songs.json';
+var tracksRequest = new XMLHttpRequest();
+tracksRequest.open('GET', tracksRequestURL);
+tracksRequest.responseType = 'json';
+tracksRequest.send();
 
-window.onload = function () {
-  createPlaylist();
+tracksRequest.onload = function() {
+  var tracks = tracksRequest.response;
+  createPlaylist(tracks);
 }
 
-function createPlaylist() {
-  var tracks = ['mean.mp3', 'onMyWay.mp3'];
-  var rootElement = document.getElementById('playlist');
 
+
+function createPlaylist(tracks) {
+  var rootElement = document.getElementById('playlist');
+  
   tracks.forEach(track => {
-    var element = document.createElement('li');
-    element.textContent = track;
+    var element = createPlaylistItem(track);
     element.onclick = function() {
       var audio = document.getElementsByTagName("audio")[0];
       if (audio.paused) {
@@ -21,4 +27,28 @@ function createPlaylist() {
     }
     rootElement.appendChild(element);
   });
+}
+
+function createPlaylistItem(track) {
+  var liElement = document.createElement('li');
+  var playlistItemElement = document.createElement('div');
+  playlistItemElement.className = "playlistItem"
+
+  var playlistNumberElement = document.createElement('span');
+  playlistNumberElement.className = "playlistNumber"
+  playlistNumberElement.textContent = track.number
+
+  var playlistTitleElement = document.createElement('span');
+  playlistTitleElement.className = "playlistTitle"
+  playlistTitleElement.textContent = track.name
+
+  var playlistLengthElement = document.createElement('span');
+  playlistLengthElement.className = "playlistLength"
+  playlistLengthElement.textContent = track.length
+
+  liElement.appendChild(playlistItemElement);
+  playlistItemElement.appendChild(playlistNumberElement);
+  playlistItemElement.appendChild(playlistTitleElement);
+  playlistItemElement.appendChild(playlistLengthElement);
+  return liElement;
 }
