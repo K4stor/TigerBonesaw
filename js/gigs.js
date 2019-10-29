0bf46d874c8a1b4f20fdc1bb838938965d81b682
Carsten Przyluczky <carsten.przyluczky@check24.de>
2019-10-29 20:54:16 +0100

    dated gigs are colored now

--- js/gigs.js
+++ js/gigs.js
@@ -1,6 +1,17 @@
 var requestURL = './json/gigs.json';
 var request = new XMLHttpRequest();
 
+//Creating a Prototype 
+Date.prototype.ddmmyyyy = function() {
+   //Grab each of your components
+   var yyyy = this.getFullYear().toString();
+   var MM = (this.getMonth()+1).toString();
+   var dd  = this.getDate().toString();
+
+   //Returns your formatted result
+  return (dd[1]?dd:"0"+dd[0]) + '.' + (MM[1]?MM:"0"+MM[0]) + '.' + yyyy;
+};
+
 function fireRequest() {
     request.open('GET', requestURL);
     request.responseType = 'json';
@@ -24,16 +35,29 @@ function createGigRow(gig, gigRootElement) {
   var gigRowElement = document.createElement('div');
   gigRowElement.className = "gig-row";
 
+  let today = new Date();
+  let date = new Date(Date.parse(gig.date));
+  let isInPast = date < today;
+
   var dateElement = document.createElement('div');
   dateElement.className = "gig-date";
-  dateElement.textContent = gig.date;
+  if (isInPast) {
+    dateElement.className += " gig-past";
+  }
+  dateElement.textContent = date.ddmmyyyy();
 
   var titleElement = document.createElement('div');
   titleElement.className = "gig-title";
+  if (isInPast) {
+    titleElement.className += " gig-past";
+  }
   titleElement.textContent = gig.title;
 
   var locationElement = document.createElement('div');
   locationElement.className = "gig-location";
+  if (isInPast) {
+    locationElement.className += " gig-past";
+  }
   locationElement.textContent = gig.location;
 
   var linkElement = document.createElement('a');
@@ -43,6 +67,9 @@ function createGigRow(gig, gigRootElement) {
 
   var linkButtonElement = document.createElement('div');
   linkButtonElement.className = "gig-link";
+  if (isInPast) {
+    linkButtonElement.className += " gig-past";
+  }
   linkButtonElement.appendChild(linkElement);
 
   gigRowElement.appendChild(dateElement);
